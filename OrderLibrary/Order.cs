488f31942aad5819cda2541ee243ebc5e4f3ebf4
bhargob <bhargob.borah15@gmail.com>
2020-06-13 15:10:30 +0530

    Order class added

---
+++ OrderLibrary/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderLibrary
+{
+    public abstract class Order
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+
+        public Order(int id, string productName)
+        {
+            this.Id = id;
+            this.ProductName = productName;
+        }
+
+        // Abstract method for implementation
+        public abstract void GenerateOrder(string paymentType);
+    }
+}
